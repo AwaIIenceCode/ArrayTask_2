24da37387f41d1d3e63e8e3280c854834d7a303c
AwallenceCode <awallencecode@gmail.com>
2026-03-02 13:09:47 +0200

    Implement initial structure for array manipulation functions in ArrayTask_2.js

---
+++ ArrayTask_2.js
@@ -0,0 +1,36 @@
+// function for creating random numbers in an array
+function createArray()
+{
+
+}
+
+// function for search even numbers in an array
+function filtreEvenNumbersInArray()
+{
+
+}
+
+// function for search odd numbers in an array
+function filtreOddNumbersInArray()
+{
+    
+}
+
+// function for find sum even numbers in an array
+function findSumEvenNumbersInArray()
+{
+
+}
+
+// function for find sum odd numbers in an array
+function findSumOddNumbersInArray()
+{
+
+}
+
+function main()
+{
+
+}
+
+main();
