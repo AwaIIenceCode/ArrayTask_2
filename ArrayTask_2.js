a70040fa2f029c48d03bd22d407ad55a27848025
AwallenceCode <awallencecode@gmail.com>
2026-03-02 14:07:19 +0200

    Refactor array manipulation functions to include user input for filtering even and odd numbers, and add display functions for both in ArrayTask_2.js

--- ArrayTask_2.js
+++ ArrayTask_2.js
@@ -1,7 +1,7 @@
 // function for creating random numbers in an array
 function createArray(userArr, min, max)
 {
-    for(let i = 0; i < userArr.length; i++)
+    for (let i = 0; i < userArr.length; i++)
     {
         userArr[i] = Math.floor(Math.random() * (max - min + 1)) + min;
     }
@@ -14,17 +14,50 @@ function writingArray(userArr)
 }
 
 // function for search even numbers in an array
-function filtreEvenNumbersInArray()
+function filtreEvenNumbersInArray(userArr)
 {
+    const evenNumbersArray = [];
 
+    for (let i = 0; i < userArr.length; i++)
+    {
+        if (userArr[i] % 2 === 0)
+        {
+            evenNumbersArray[i] = userArr[i];
+        }
+    }
+
+    return evenNumbersArray;
+}
+
+// function for show even numbers in an array
+function writingEvenNymbersArray()
+{
+    filtreEvenNumbersInArray();
+    console.log(userArr.join(", "));
 }
 
 // function for search odd numbers in an array
-function filtreOddNumbersInArray()
+function filtreOddNumbersInArray(userArr)
 {
-    
+    const oddNumbersArray = [];
+
+    for (let i = 0; i < userArr.length; i++)
+    {
+        if (userArr[i] % 2 !== 0)
+        {
+            oddNumbersArray[i] = userArr[i];
+        }
+    }
+
+    return oddNumbersArray;
 }
 
+// function for show odd numbers in an array
+function writingOddNumbersArray()
+{
+    filtreOddNumbersInArray();
+    console.log(userArr.join(", "));
+}
 // function for find sum even numbers in an array
 function findSumEvenNumbersInArray()
 {
@@ -44,6 +77,12 @@ function main()
 
     createArray(userArr, -100, 100);
     writingArray(userArr);
+    filtreEvenNumbersInArray(userArr);
+    filtreOddNumbersInArray(userArr);
+    findSumEvenNumbersInArray(userArr);
+    findSumOddNumbersInArray(userArr);
+    writingEvenNymbersArray();
+    writingOddNumbersArray();
 }
 
 main();
